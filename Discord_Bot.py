b0ec1d1d1991bfe4150d75c155365e7e9f36f18e
agalfred <72280365+agalfred@users.noreply.github.com>
2021-02-22 13:40:18 -0800

    Made major changes. Added .env support. Created RedditCollector module. Updated .gitignore to add ./path/

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -1,1 +1,18 @@
+import os
+import discord
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from RedditCollector import RedditCollector
+
+
+data_collector = RedditCollector(client_id=os.getenv("REDDIT_ID"), 
+                                client_secret=os.getenv("REDDIT_SECRET"), 
+                                user_agent=os.getenv("REDDIT_UA"), 
+                                subreddits_list=['memes'], 
+                                limit=10,
+                                username='',
+                                password='')
+data_collector.collect_data()
